bab772f14655913ef238558a89f49644967fffe6
anasraza07 <ar470684@gmail.com>
2024-06-30 03:02:19 +0500

    problem 06 done

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // Problem 01: Make a function that convert temperature from celcius (°C) to fahrenheit (°F) and from fahrenheit (°F) to celcius (°C).
 // Fahrenheit = (Celcius * 9/5) + 32
 // Celcius = (fahrenheit - 32) * 5/9
-function celciusToFahrenheit(temperature) {
-  return `${(temperature * 9) / 5 + 32}°F`;
+function convertToFahrenheit(celsius) {
+  return `${(celsius * 9) / 5 + 32}°F`;
 };
 
-function fahrenheitToCelcius(temperature) {
-  return `${(temperature - 32) * 5 / 9}°C`;
+function convertToCelcius(fahrenheit) {
+  return `${(fahrenheit - 32) * 5 / 9}°C`;
 };
 
-console.log(celciusToFahrenheit(-5))
-console.log(fahrenheitToCelcius(86))
+console.log(convertToFahrenheit(-5))
+console.log(convertToCelcius(86))
 
 // Problem 02: Make a calculator
 // 'calculator/'
@@ -24,3 +24,54 @@ if ((age <= 5 || age >= 65) && !holiday) {
 } else {
   console.log('No Discount');
 }
+
+// Problem 04: A coin-flip game
+const randomNumber = Math.random();
+let result;
+
+if (randomNumber > 0.5) {
+  result = 'heads';
+} else {
+  result = 'tails';
+}
+
+const guess = 'heads';
+if (guess === result) {
+  console.log('You win!');
+} else {
+  console.log('You lose!')
+}
+
+// Problem 05: Create a function 'convertTemperature(degrees, unit)' that takes a number and a unit ('C' or 'F'), and converts it into the other unit. convertTemperature (25, 'C') => '77F' convertTemperature (86, 'F') => '30C'
+function convertTemperature(degrees, unit) {
+  if (unit === 'F') {
+    return convertToCelcius(degrees);
+  } else {
+    return convertToFahrenheit(degrees);
+  }
+};
+console.log(convertTemperature(25, 'C'));
+console.log(convertTemperature(86, 'F'));
+
+// Problem 06: Create a function convertLength(length, from, to) that takes a number and a unit ('km' or 'miles') and converts the length to another unit ('km' or 'miles'). Note: 1 mile = 1.6 km (approximately).
+// - convertLength(50, 'miles', 'km') => '80 km'
+// - convertLength(32, 'km', 'miles') => '20 miles' 
+// - convertLength(50, 'km', 'km') => '50 km'
+function convertLength(length, from, to) {
+  let result;
+  if (from === 'km' && to === 'miles') {
+    result = length / 1.6
+  } else if (from === 'miles' && to === 'km') {
+    result = length * 1.6
+  } else if (from === to) {
+    result = length
+  } else {
+    return 'Invalid Values';
+  }
+
+  return `${result} ${to}`;
+}
+
+console.log(convertLength(50, 'miles', 'km'));
+console.log(convertLength(32, 'km', 'miles'));
+console.log(convertLength(50, 'km', 'km'));
